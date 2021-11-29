01dda698fe7e4f36c716b4a71027b2265f5485a4
Isaac Hsu <16hsuisaac@gmail.com>
2021-11-28 16:48:24 -0800

    found bugs in generated code, no longer continuing

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
+import fetch from "node-fetch";
+
+const { doesNotMatch } = require('assert');
 var DealersAndVehicles = require('dealers_and_vehicles');
 
 var apiInstance = new DealersAndVehicles.DataSetApi();
+
+let datasetId = '';
+var opts = {};
 
 apiInstance.getDataSetId(function(error, data, response) {
   if (error) {
     console.error(error);
     return;
   }
-  console.log('data', data)
+  datasetId = data.datasetId
+})
+
+fetch(`https://api.coxauto-interview.com/api/${datasetId}/answer`, {
+  method: 'POST',
+  headers: {
+    'Content-Type':'application/json',
+  },
+  body: JSON.stringify(datasetId),
 })
